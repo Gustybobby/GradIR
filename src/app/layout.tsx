6dbcc3d771ac4bfbb903bf74b0eef979735d1b5e
Gustybobby <napattatiyakaroonwong@gmail.com>
2026-05-06 10:08:34 +0800

    feat: rename app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Graduate Program Search",
+  title: "GradIR",
   description:
     "Institution search by research interests for Web Information Retrieval 2026 course, THU",
 };
--- src/client/home/Home.tsx
+++ src/client/home/Home.tsx
@@ -5,7 +5,7 @@ export function Home() {
     <main className="w-full h-screen flex items-center justify-center">
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-4xl md:text-6xl font-extrabold tracking-tight text-balance text-center">
-          Graduate Program Search
+          GradIR
         </h1>
         <HomeSearchBox />
       </div>
